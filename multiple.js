9bee4b62e79e0bffc5f9cc3485ba78738d5d8409
Farzanayesmin21 <yesmin21212@gmail.com>
2025-10-14 19:32:55 +0600

    added multiple

---
+++ multiple.js
@@ -0,0 +1,11 @@
+function multiple(num){
+    if (num % 3 === 0 && num % 5 === 0){
+        console.log("Divisible by both");
+
+    }
+        
+    else{
+        console.log(" Not divisible by both");
+    }
+}
+multiple(9)
